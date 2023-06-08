d0dd3d0b7ca3943ef902f321d0a60e8aab1aab05
othniel2471 <othnielomaidu@gmail.com>
2023-06-08 16:23:42 +0100

    initialized addItem function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import './styles/main.scss';
+// import Todo, {  } from './modules/display.js';
 import Todo from './modules/display.js';
 
 Todo.initialize();
+window.addEventListener('DomContentLoaded', () => {
+  Todo.editItem();
+});
