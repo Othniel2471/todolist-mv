1e694c0b2682ec1de35b07cc633facdc168c3997
othniel2471 <othnielomaidu@gmail.com>
2023-06-09 00:18:22 +0100

    added a function to listen to change events on the checkboxes

---
+++ src/modules/taskStatus.js
@@ -0,0 +1,15 @@
+function checkItem(e, Todo) {
+  const element = e.target.closest('.list');
+  const index = Array.from(Todo.listContainer.children).indexOf(element);
+  Todo.todoLists[index].completed = e.target.checked;
+  const editBtn = element.querySelector('.edit-btn');
+  if (Todo.todoLists[index].completed === false) {
+    editBtn.classList.remove('checkActive');
+    localStorage.setItem('todoLists', JSON.stringify(Todo.todoLists));
+  } else {
+    editBtn.classList.add('checkActive');
+    localStorage.setItem('todoLists', JSON.stringify(Todo.todoLists));
+  }
+}
+
+export default checkItem;
